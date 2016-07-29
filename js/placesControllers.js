0bb8cf47cea0d2d634c090d6dfc95e84e122da49
Cory Kleiser <moku@Corys-MacBook-Pro.local>
2016-07-29 14:16:24 -0400

    rearranged code so geoLocation happens on homepage

--- js/placesControllers.js
+++ js/placesControllers.js
@@ -2,8 +2,62 @@
 angular.module(`tadooApp.controller`, [])
     
     .controller(`CatController`, [`$scope`, function ($scope) {
-        //category controller
-}])
+        let request;
+        let userLocation;
+
+        //Set Up Google Map and Location
+
+        $scope.places = [];
+
+
+
+        navigator.geolocation.getCurrentPosition(function (position) {
+            let LatLng = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+            request = {
+                location: LatLng,
+                radius: `3000`,
+                types: [`restaurant`]
+            };
+
+
+            const map = new google.maps.Map($(`#mapContainer`), {
+                center: LatLng,
+                zoom: 14,
+            });
+
+            const service = new google.maps.places.PlacesService(map);
+
+            service.nearbySearch(request, function(results, status) {
+                if (status == google.maps.places.PlacesServiceStatus.OK) {
+
+                    //TODO: cycle through place results and filter needed information
+                    for (var i = 0; i < results.length; i++) {
+                        var place = results[i];
+                        //TODO: filter results here
+                        if (place.rating === undefined){
+                            continue;
+                        }
+                        genInfo = {
+                            restaurantName: place.name,
+                            googleRating: place.rating
+                        };
+
+                        $scope.places.push(genInfo);
+                        // If the request succeeds, draw the place location on
+                        // the map as a marker, and register an event to handle a
+                        // click on the marker.
+                    }
+                    console.log($scope.places);
+                }
+                else {
+                    console.log(status);
+                }
+            });
+        });
+    }])
     .controller(`ListController`, [`$scope`, `$routeParams`, function ($scope, $routeParams) {
         $scope.itemId = $routeParams.itemId;
         if ($scope.itemId == 0){
@@ -27,61 +81,5 @@ angular.module(`tadooApp.controller`, [])
             $scope.catHeader = `TaChill`;
         }
         console.log($scope.category);
-        let request;
-        //Set Up Google Map and Location
-
-
-            const callback = function(results, status) {
-                if (status == google.maps.places.PlacesServiceStatus.OK) {
-
-                    //TODO: cycle through place results and filter needed information
-                    const places = [];
-                    for (var i = 0; i < results.length; i++) {
-                        var place = results[i];
-                        //TODO: filter results here
-                        if (place.rating === undefined){
-                            continue;
-                        }
-                        genInfo = {
-                            restaurantName: place.name,
-                            googleRating: place.rating
-                        };
-
-                        places.push(genInfo);
-                        // If the request succeeds, draw the place location on
-                        // the map as a marker, and register an event to handle a
-                        // click on the marker.
-                    }
-
-                    console.log(places);
-                    return places;
-
-                }
-                else {
-                    console.log(status);
-                }
-            };
-
-            navigator.geolocation.getCurrentPosition(function (position) {
-                let LatLng = {
-                    lat: position.coords.latitude,
-                    lng: position.coords.longitude
-                };
-                request = {
-                    location: LatLng,
-                    radius: 25600,
-                    types: [$scope.category]
-                };
-
-
-                const map = new google.maps.Map($(`#mapContainer`), {
-                    center: LatLng,
-                    zoom: 14,
-                });
-
-                const service = new google.maps.places.PlacesService(map);
-
-                $scope.places = service.nearbySearch(request, callback);
-            });
-        }]);
+    }]);
 //TODO: Set up controller to find User Location
